8e8a6e71b88f8077a05ad071af01fa037c1acf31
hilton khadka <hilton.khadka.mdl@gmail.com>
2024-04-09 22:23:54 +0200

    --amend

--- src/components/Header.js
+++ src/components/Header.js
@@ -127,7 +127,7 @@ export const Header = () => {
                             rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 
                             md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
                 <li>
-                  <NavLink to="/" className={({ isActive }) => { return isActive ? activeLink : inactiveLink }} end>!Home!</NavLink>
+                  <NavLink to="/" className={({ isActive }) => { return isActive ? activeLink : inactiveLink }} end>Home</NavLink>
                 </li>
                 <li>
                   <NavLink to="/movies/popular" className={({ isActive }) => { return isActive ? activeLink : inactiveLink }}>Popular</NavLink>
